aaa6478b86c4453d86f0ba898b48ac155d0fe04e
cliff <dunderjeep@gmail.com>
2025-02-05 12:10:43 -0500

    added homeherocyse1008 component

--- src/sections/home/view/home-view.jsx
+++ src/sections/home/view/home-view.jsx
@@ -5,7 +5,7 @@ import Stack from '@mui/material/Stack';
 import { BackToTop } from 'src/components/animate/back-to-top';
 import { ScrollProgress, useScrollProgress } from 'src/components/animate/scroll-progress';
 
-import { HomeHero } from '../home-hero';
+import { HomeHeroCYSE1008 } from '../home-hero-cyse1008';
 import { HomeFAQs } from '../home-faqs';
 import { HomeZoneUI } from '../home-zone-ui';
 import { HomeMinimal } from '../home-minimal';
@@ -32,7 +32,7 @@ export function HomeView() {
 
       <BackToTop />
 
-      <HomeHero />
+      <HomeHeroCYSE1008 />
 
       <Stack sx={{ position: 'relative', bgcolor: 'background.default' }}>
         <HomeMinimal />
